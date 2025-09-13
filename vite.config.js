4d35ee9e62e9a015ca3b5e65ff824ee538cbf5ff
Your Name <you@example.com>
2025-09-13 03:43:24 +0300

    remove hard coded stuff

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// If deploying to GitHub Pages at https://username.github.io/repo-name/
-// set REPO_NAME=repo-name (GitHub Actions will export this) so assets use correct subpath.
-const repoName = process.env.Algorithm_Project || '';
-const base = repoName ? `/${repoName}/` : '/';
-
 // https://vite.dev/config/
 export default defineConfig({
-  base,
+  base: '/Algorithm_Project/',
   plugins: [react()],
 })
